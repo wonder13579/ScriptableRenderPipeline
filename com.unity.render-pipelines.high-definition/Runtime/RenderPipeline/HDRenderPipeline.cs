820c5abef9b1d1967fe4a45941a060599054dee1
RSlysz <remi@unity3d.com>
2019-05-03 13:47:20 +0200

    Move Implementation in HDRP to dedicated file

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
@@ -4,15 +4,10 @@ using System;
 using System.Diagnostics;
 using System.Linq;
 using UnityEngine.Experimental.GlobalIllumination;
-using UnityEngine;
-using UnityEngine.Rendering.LookDev;
-using UnityEngine.SceneManagement;
-using UnityEngine.Rendering.LookDev;
-using LookDevEnvironment = UnityEngine.Rendering.LookDev.CustomRenderSettings;
 
 namespace UnityEngine.Experimental.Rendering.HDPipeline
 {
-    public class HDRenderPipeline : UnityEngine.Rendering.RenderPipeline, IDataProvider
+    public partial class HDRenderPipeline : UnityEngine.Rendering.RenderPipeline
     {
         public const string k_ShaderTagName = "HDRenderPipeline";
         const string k_OldQualityShadowKey = "HDRP:oldQualityShadows";
@@ -3503,76 +3498,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             {
                 var colorBuffer = hdCamera.GetCurrentFrameRT((int)HDCameraFrameHistoryType.ColorBufferMipChain);
                 VFX.VFXManager.SetCameraBuffer(hdCamera.camera, VFX.VFXCameraBufferTypes.Color, colorBuffer, 0, 0, hdCamera.actualWidth, hdCamera.actualHeight);
-            }
-        }
-
-        struct LookDevDataForHDRP
-        {
-            public HDAdditionalCameraData additionalCameraData;
-            public VisualEnvironment visualEnvironment;
-            public HDRISky sky;
-            public Volume volume;
-        }
-
-        LookDevEnvironment IDataProvider.GetEnvironmentSetup()
-            => new LookDevEnvironment()
-            {
-                defaultReflectionMode = UnityEngine.Rendering.DefaultReflectionMode.Custom,
-                customReflection = null,
-                skybox = null,
-                ambientMode = AmbientMode.Trilight
-            };
-
-        void IDataProvider.FirstInit(StageRuntimeInterface SRI)
-        {
-            Camera camera = SRI.camera;
-            camera.allowHDR = true;
-
-            var additionalData = camera.gameObject.AddComponent<HDAdditionalCameraData>();
-            additionalData.clearColorMode = HDAdditionalCameraData.ClearColorMode.Color;
-            additionalData.clearDepth = true;
-            additionalData.backgroundColorHDR = camera.backgroundColor;
-            additionalData.volumeAnchorOverride = camera.transform;
-
-            GameObject volumeGO = SRI.AddGameObject(persistent: true);
-            volumeGO.name = "SkyManagementVolume";
-            Volume volume = volumeGO.AddComponent<Volume>();
-            volume.isGlobal = true;
-            volume.priority = float.MaxValue;
-            VolumeProfile profile = ScriptableObject.CreateInstance<VolumeProfile>();
-            volume.profile = profile;
-            VisualEnvironment visualEnvironment = profile.Add<VisualEnvironment>();
-            HDRISky sky = profile.Add<HDRISky>();
-
-            SRI.SRPData = new LookDevDataForHDRP()
-            {
-                additionalCameraData = additionalData,
-                visualEnvironment = visualEnvironment,
-                sky = sky,
-                volume = volume
-            };
-
-
-            //temp for debug: show component in scene hierarchy
-            //UnityEngine.SceneManagement.SceneManager.MoveGameObjectToScene(camera.gameObject, GameObject.Find("Main Camera").scene);
-            //camera.gameObject.hideFlags = HideFlags.None;
-            //UnityEngine.SceneManagement.SceneManager.MoveGameObjectToScene(volumeGO, GameObject.Find("Main Camera").scene);
-            //volumeGO.hideFlags = HideFlags.None;
-        }
-
-        void IDataProvider.UpdateSky(Camera camera, Cubemap skybox, StageRuntimeInterface SRI)
-        {
-            LookDevDataForHDRP data = (LookDevDataForHDRP)SRI.SRPData;
-            if(skybox == null)
-            {
-                data.visualEnvironment.skyType.Override((int)0); //Skytype.None do not really exist
-                data.additionalCameraData.clearColorMode = HDAdditionalCameraData.ClearColorMode.Color;
-            }
-            else
-            {
-                data.visualEnvironment.skyType.Override((int)SkyType.HDRISky); 
-                data.sky.hdriSky.Override(skybox);
-                data.additionalCameraData.clearColorMode = HDAdditionalCameraData.ClearColorMode.Sky;
             }
         }
     }
